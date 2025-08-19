447b6b9467ed3c33883d9dd38eef447415041152
Vaibhav Srivastava <123542549+Vaibhav7398@users.noreply.github.com>
2025-08-19 10:45:11 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,28 @@
-import { Html, Main, NextScript, Head } from "next/document";
+import { Html, Head, Main, NextScript, DocumentProps, DocumentInitialProps } from "next/document";
+import NextDocument from "next/document";
 
-const document = () => (
-  <Html>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="allow"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <body>
-      <div id="portal"></div>
-      <Main />
-      <NextScript />
-    </body>
-  </Html>
-);
+class Document extends NextDocument<DocumentProps, DocumentInitialProps> {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          {/* Portal root for modals or tooltips */}
+          <div id="portal"></div>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-export default document;
+export default Document;
