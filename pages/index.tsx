20bfe01403278ad5db55c1fddb6d9b084991e1b4
Vaibhav Srivastava <123542549+Vaibhav7398@users.noreply.github.com>
2025-08-19 10:44:19 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from "next";
-
+import { NextPage } from "next";
 import Home from "@/modules/home/components/Home";
 
-const HomePage: NextPage = () => {
-  return <Home />;
-};
+// HomePage component - entry point for the home route
+const HomePage: NextPage = () => <Home />;
 
 export default HomePage;
